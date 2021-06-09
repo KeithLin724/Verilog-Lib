13ef19185b8a0a9223209629969b54bee6c28c05
KeithLin724 <38067890+KeithLin724@users.noreply.github.com>
2021-06-09 19:45:53 +0800

    Update SS_display_ultra.v

--- Keyboard_adder/SS_display_ultra.v
+++ Keyboard_adder/SS_display_ultra.v
@@ -18,6 +18,45 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+module Seven_segment_display_ultra(
+    input [7:0] light_on ,
+    input [3:0] BCD ,
+    input DP,  
+    
+    output [7:0]light_code,// lightcode on
+    output reg [6:0] decode ,
+    output DP_out
+    
+    );
+    assign light_code = ~light_on ;  
+    assign DP_out = ~DP;
+    
+    always @(BCD) begin 
+        case (BCD)
+        4'b0000:  decode = ~(7'b1111110);
+        4'b0001:  decode = ~(7'b0110000);
+        4'b0010:  decode = ~(7'b1101101);
+        4'b0011:  decode = ~(7'b1111001);
+        4'b0100:  decode = ~(7'b0110011);
+        4'b0101:  decode = ~(7'b1011011);
+        4'b0110:  decode = ~(7'b0011111);
+        4'b0111:  decode = ~(7'b1110000);
+        4'b1000:  decode = ~(7'b1111111);
+        4'b1001:  decode = ~(7'b1110011); // 9 
+        4'b1010:  decode = ~(7'b0000001); // 10 is - 
+        4'b1011:  decode = ~(7'b0111110); // 11 is u  
+        default:   decode = ~(7'b0000000);
+        /*4'b1010:  decode = ~(7'b0001101);
+        4'b1011:  decode = ~(7'b0011001);
+        4'b1100:  decode = ~(7'b0100011);
+        4'b1101:  decode = ~(7'b1001011);
+        4'b1110:  decode = ~(7'b0001111);*/
+       // 4'b1111:  decode = ~(7'b0000000);
+        endcase 
+    end    
+endmodule
+
 module Div_n_Frequency(
     input  clk,
    // input  rst_n,
@@ -42,6 +81,7 @@ module SS_display_ultra(//1010 is -
     output [6:0] decode ,
     output DP_out
     );
+    
     wire [3:0] array [7:0] ; 
     wire O_clk ; 
     reg [7:0] Light_sigle = 8'b00000001;
@@ -76,13 +116,13 @@ module SS_display_ultra(//1010 is -
     end 
       
     Seven_segment_display_ultra SS_ultra(
-    .light_on(Light_sigle),
-    .BCD(array[Count]),
-    .DP(DP),
-    
-    .light_code(light_code),// lightcode on
-    .decode(decode) ,
-    .DP_out(DP_out)
+        .light_on(Light_sigle),
+        .BCD(array[Count]),
+        .DP(DP),
+
+        .light_code(light_code),// lightcode on
+        .decode(decode) ,
+        .DP_out(DP_out)
     );
     
 endmodule
